24f7c7375f85060f3f761b39239dbc897ffa3fde
Jon <jong649@gmail.com>
2020-02-20 00:22:54 -0800

    Added fetch to use jsonplaceholder api for users list

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 class App extends Component {
@@ -14,7 +13,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ robots: robots });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = event => {
@@ -26,13 +27,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
