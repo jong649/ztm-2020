7754849312e29dc55fcfbcab652835546c1fb0c7
Jon <jong649@gmail.com>
2020-02-20 00:10:18 -0800

    Added styles & web font

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
+import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
   }
+
+  componentDidMount() {
+    this.setState({ robots: robots });
+  }
+
   onSearchChange = event => {
     this.setState({ searchfield: event.target.value });
   };
@@ -22,7 +28,7 @@ class App extends React.Component {
     });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
